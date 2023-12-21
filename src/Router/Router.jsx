b71358e55d155f19f570b6e3329b75accb48a3a2
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-21 20:42:06 +0600

    navbar added

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root/Root";
+import Home from "../Pages/Home/Home";
+
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default Router;
