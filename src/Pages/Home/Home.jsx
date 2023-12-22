c200a5f6bb7ed00e5ff7ca130d687de65ff0a80f
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-22 12:56:53 +0600

    who use this app section/route added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../Components/Banner/Banner";
+import Used from "./Used/Used";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Used></Used>
         </div>
     );
 };
---
+++ src/Pages/Home/Used/Used.jsx
@@ -0,0 +1,11 @@
+import SectionTittle from "../../../Shared/SectionTittle";
+
+const Used = () => {
+    return (
+        <div className="my-20">
+            <SectionTittle subTittle={"Used"} tittle={"Who Use This App"}></SectionTittle>
+        </div>
+    );
+};
+
+export default Used;
