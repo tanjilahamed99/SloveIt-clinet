b47a6125ac6f4243377783d0de7ac8961e308306
Tanjil Ahamed <tanjil113355@gmail.com>
2024-01-23 21:17:28 +0600

    responsive complete

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,13 @@
 
 const About = () => {
     return (
-        <div className="container mx-auto  my-20 flex flex-col lg:flex-row  items-center gap-10">
-            <div className="lg:w-1/2">
+        <div className="container mx-auto  my-20 flex flex-col md:flex-row  items-center gap-10 px-5">
+            <div className="md:w-1/2">
                 <img className="w-full" src="https://i.ibb.co/9vYvKRL/images-16.jpg" alt="" />
             </div>
-            <div className="lg:w-1/2 space-y-3">
+            <div className="md:w-1/2 space-y-3 ">
                 <h2 className="text-[#c19d68] font-medium">Task Management Website</h2>
-                <h3 className="text-3xl font-semibold text-[#1e1e1e]">Solved It: Unleash Productivity, Simplify Tasks, Achieve More in Minutes</h3>
+                <h3 className="lg:text-3xl text-xl  font-semibold text-[#1e1e1e]">Solved It: Unleash Productivity, Simplify Tasks, Achieve More in Minutes</h3>
                 <p className="text-[#616161]">SolvedIt: Your Key to Effortless Task Mastery. Simplify, organize, and conquer your to-do list with our intuitive task management platform. Elevate your productivity journey now!</p>
                 <button className="btn btn-outline">About More</button>
             </div>
--- src/Pages/Home/NewsLatter/NewsLatter.jsx
+++ src/Pages/Home/NewsLatter/NewsLatter.jsx
@@ -1,7 +1,7 @@
 import { FaArrowRight } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
-import useAXiosPublic from "../../../Hooks/AxiosPublic/UseAxiosPublic";
+import useAXiosPublic from "../../../Hooks/AxiosPublic/useAXiosPublic";
 
 const NewsLatter = () => {
 
@@ -42,9 +42,9 @@ const NewsLatter = () => {
             <div className="hero-overlay bg-opacity-80"></div>
             <div className="hero-content w-full text-center text-neutral-content">
                 <div className="w-[60%] mx-auto">
-                    <h1 className="mb-5 text-3xl font-medium">Sing up to our newsletter for all the latest news</h1>
+                    <h1 className="mb-5 md:text-3xl text-xl font-medium">Sing up to our newsletter for all the latest news</h1>
                     <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center items-center">
-                        <input {...register("email")} placeholder="E-mail Address" className=" w-1/2 py-3 focus:border-none pl-2 text-white border-b-2 border-white bg-black bg-opacity-10" type="email" />
+                        <input {...register("email")} placeholder="E-mail Address" className=" md:w-1/2 py-3 focus:border-none pl-2 text-white border-b-2 border-white bg-black bg-opacity-10" type="email" />
                         <button type="submit"><FaArrowRight className="text-white text-xl -ml-4"></FaArrowRight></button>
                     </form>
                 </div>
